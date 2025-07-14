d3b4fa9bb37ee9c6730ef92364a6598f5e2eb4d7
chienwen661 <40230782+chienwen661@users.noreply.github.com>
2025-07-14 20:24:36 +0800

    Update data1.js

--- data1.js
+++ data1.js
@@ -43,7 +43,12 @@ var qs={
     "phone":"電話",
     "yellow":"黃色",
     "lemon":"檸檬",
-    "baseball":"棒球","page":"頁","carrot":"紅蘿蔔","mango":"芒果","pea":"豌豆","sister":"姊妹",
+    "baseball":"棒球",
+    "page":"頁",
+    "carrot":"紅蘿蔔",
+    "mango":"芒果",
+    "pea":"豌豆",
+    "sister":"姊妹",
     "eason":"陳昱瀚"
 };
 var qs = qs.slice(40).concate(qs[3],qs[6],qs[4],qs[32],qs[35],qs[38]);
